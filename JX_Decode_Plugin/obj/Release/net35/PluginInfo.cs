7222ce3fc7476dfbdf44ad0c8d9e0b01eb1e3f13
EasternDay <849919718@qq.com>
2021-10-13 13:01:31 +0800

    更新.gitignore，去除部分无用文件。

--- JX_Decode_Plugin/obj/Release/net35/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace JX_Decode_Plugin
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "JX_Decode_Plugin";
-        public const string PLUGIN_NAME = "JX_Decode_Plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
